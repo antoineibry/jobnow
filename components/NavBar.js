4e9e880e23a0a16c6593d08effc406b5bfe0c15c
antoineibry <antoine.ibry@outlook.com>
2023-08-16 11:48:19 +0300

    first

---
+++ components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <section className=" bg-graybg h-[693px] grid grid-flow-col col-span-12">
+        <div className="bg-graybg col-span-7">.</div>
+        <div className="bg-green col-span-5">.</div>
+      </section>
+    </>
+  );
+};
+
+export default NavBar;
