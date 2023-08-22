434be3bf06cc9b05af138044eb91d42c10524e5e
antoineibry <antoine.ibry@outlook.com>
2023-08-23 00:57:23 +0300

    example

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,23 +6,33 @@ const locationOptions = ["Beirut", "Zahle", "Akkar"];
 const NavBar = () => {
   return (
     <>
-      <section className=" bg-gray h-[570px] grid grid-flow-col grid-cols-12">
+      <section className=" bg-gray h-[600px] grid grid-flow-col grid-cols-12 text-black">
         <div className="bg-gray col-span-7">
-          <div className="flex flex-row w-full justify-center items-center 2xl:container mx-auto mt-7">
+          <div className="flex flex-row w-full justify-center items-center 2xl:container mx-auto mt-11">
             <div className="flex items-center">
-              <Image
-                src="/Section1/logo.svg"
-                width={120}
-                height={50}
-                alt="logo"
-                className="mx-12"
-              />
+              <a href="#">
+                <Image
+                  src="/Section1/logo.svg"
+                  width={120}
+                  height={50}
+                  alt="logo"
+                  className="mx-12"
+                />
+              </a>
             </div>
             <ul className="flex flex-row justify-between font-medium text-s">
-              <li className="pr-16 min-w-fit">Home</li>
-              <li className="pr-16 min-w-fit">Job</li>
-              <li className="pr-16 min-w-fit">About Us</li>
-              <li className="pr-16 min-w-fit">Contact</li>
+              <li className="pr-16 min-w-fit">
+                <a href="#">Home</a>
+              </li>
+              <li className="pr-16 min-w-fit">
+                <a href="#">Job</a>
+              </li>
+              <li className="pr-16 min-w-fit">
+                <a href="#">About Us</a>
+              </li>
+              <li className="pr-16 min-w-fit">
+                <a href="#">Contact</a>
+              </li>
             </ul>
           </div>
 
@@ -35,7 +45,7 @@ const NavBar = () => {
               lacus non orci euismod vestibulu virae ut ex. Quisque ut arcu at
               lectus tristique auctor sit amet at turpis.
             </div>
-            <div className=" rounded-3xl bg-white p-5 mx-32 flex justify-between flex-row w-3/4 mt-10">
+            <div className=" rounded-2xl bg-white py-3 px-7 mx-32 flex justify-between flex-row w-3/4 mt-10">
               <div className="flex-row flex">
                 <Image
                   src="/Section1/search-icon.png"
@@ -59,7 +69,7 @@ const NavBar = () => {
                 />
                 <select
                   id="countries"
-                  class="bg-gray-50  text-gray-900 text-m rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="text-gray-900 text-m rounded-lg w-full"
                 >
                   <option selected>Location</option>
                   <option value="US">United States</option>
@@ -69,18 +79,18 @@ const NavBar = () => {
                 </select>
               </div>
 
-              <div>
-                <button className="px-8 py-3 rounded-xl bg-green text-white font-medium">
+              <div className="py-0">
+                <button className="px-8 py-2 rounded-xl bg-green text-white font-medium">
                   Search
                 </button>
               </div>
             </div>
           </div>
         </div>
-        <div className="bg-green col-span-5 rounded-bl-3xl">
-          <div className="flex flex-row w-full justify-center items-center 2xl:container mx-auto mt-4 ">
-            <div className="text-white mr-6 font-medium">Sign In</div>
-            <div className="flex-row flex items-center bg-white rounded-xl py-1 px-3 font-semibold">
+        <div className="bg-green col-span-5 rounded-bl-3xl relative">
+          <div className="flex flex-row w-full justify-center items-center 2xl:container mx-auto mt-8">
+            <div className="text-white mr-10 font-medium">Sign In</div>
+            <div className="flex-row flex items-center bg-white rounded-xl px-4 font-semibold">
               <Image
                 src="/Section1/door-icon.png"
                 width={20}
@@ -90,6 +100,25 @@ const NavBar = () => {
               <div>Create Account</div>
             </div>
           </div>
+          <Image
+            src="/Section1/icon1.png"
+            width={70}
+            height={70}
+            className="absolute top-32 left-24"
+          />
+          <Image
+            src="/Section1/icon2.png"
+            width={70}
+            height={70}
+            className="absolute top-64 right-24"
+          />
+
+          <Image
+            src="/Section1/icon3.png"
+            width={70}
+            height={70}
+            className="absolute top-96 left-16"
+          />
         </div>
       </section>
     </>
